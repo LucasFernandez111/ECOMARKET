fc1722659b04768cede993daa458c7294aff9e9d
Lucas <flucasyoel@gmail.com>
2024-02-21 14:58:29 -0300

    Refactor and Clean Code

---
+++ src/components/Example.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "./Card";
+
+const listProducts = [
+  {
+    id: 1,
+    name: "Gaseosa Cunnington sabor Suave 1.5L",
+    href: "#",
+    imageSrc:
+      "https://carrefourar.vtexassets.com/arquivos/ids/359527/7790639003819_E02.jpg?v=638258156831630000",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$100",
+  },
+  {
+    id: 1,
+    name: "CocaCola ZERO 500ML",
+    href: "#",
+    imageSrc:
+      "https://www.coca-cola.com/content/dam/onexp/ar/es/coca-cola/coca-cola-zero-v1.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+  },
+  // More products...
+];
+
+export default function Example() {
+  return (
+    <div className="bg-gray-800 flex gap-3">
+      {listProducts.map((products) => {
+        return (
+          <Card
+            key={products.name}
+            image={products.imageSrc}
+            name={products.name}
+            price={products.price}
+          ></Card>
+        );
+      })}
+    </div>
+  );
+}
