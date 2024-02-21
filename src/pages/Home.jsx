146885f115c0d56d494a9c4a829469759b355e71
Lucas <flucasyoel@gmail.com>
2024-02-21 14:57:36 -0300

    Create Home - component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div className=" bg-lime-200 w-screen h-screen">
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
