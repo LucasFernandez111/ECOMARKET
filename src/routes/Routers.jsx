0c036049aef6875168dde9241fe2b2fc55b14361
Lucas <flucasyoel@gmail.com>
2024-02-21 15:51:39 -0300

    Update

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Example from "../components/Example";
 import Home from "../pages/Home";
+import ListSections from "../components/ListSections";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
-      <Route path="/Example" element={<Example />} />
+      <Route path="/Example" element={<ListSections />} />
     </Routes>
   );
 };
