07aa9f94e34344bde079357faa36bd02663c2cd0
Lucas <flucasyoel@gmail.com>
2024-02-21 15:51:34 -0300

    Add List Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav";
+import ListSections from "../components/ListSections";
 
 const Home = () => {
   return (
-    <div className=" bg-lime-200 w-screen h-screen">
+    <div className=" bg-lime-200 w-screen h-screen ">
       <Nav />
+      <ListSections></ListSections>
     </div>
   );
 };
