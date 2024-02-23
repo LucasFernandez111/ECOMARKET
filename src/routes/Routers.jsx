4007925ae8faa29159e05bc1651f709fa0b9df8a
Yasmin <rallas674@gmail.com>
2024-02-22 22:10:01 -0300

    changes

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Example from "../components/Example";
 import Home from "../pages/Home";
 import ListSections from "../components/ListSections";
+import Component from "../components/CarouselHome";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Example" element={<ListSections />} />
+      <Route path="/prueba" element={<Component />} />
     </Routes>
   );
 };
