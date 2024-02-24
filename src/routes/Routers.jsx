38de33fe6273103f3373d65ffd1c00939cb278e9
lucas fernandez <flucasyoel@outlook.es>
2024-02-23 21:15:38 -0300

    Create - Page Login and Add Route '/login'

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ListSections from "../components/ListSections";
-import Component from "../components/CarouselHome";
+import CarouselHome from "../components/CarouselHome";
+import Login from "../pages/Login";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Example" element={<ListSections />} />
-      <Route path="/prueba" element={<Component />} />
+      <Route path="/prueba" element={<CarouselHome />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
