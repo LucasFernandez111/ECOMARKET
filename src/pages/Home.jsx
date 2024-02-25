e4f4291cf96aae699d2fbd4b5d41f86a1a334274
yasminsitaa <rallas674@gmail.com>
2024-02-25 01:53:16 -0300

    new commit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,26 @@ import Nav from "../components/Nav";
 import ListSections from "../components/ListSections";
 import { BiSolidLeaf as IconLeaf } from "react-icons/bi";
 import ScrollCard from "../components/ScrollCard";
-import oferta from "../assets/inaguracion.png"
+import oferta from "../assets/inaguracion.png";
+import Carousel from "../components/Carousel";
 
 const Home = () => {
   return (
     <div className=" bg-lime-50 w-screen h-screen ">
-      <header className="mb-24 sticky">
+      <header className="mb-24 sticky z-[1000]">
         <Nav />
       </header>
 
       <main className="flex flex-row">
-        <aside className="mr-44 sticky z-20 ">
+        <aside className="mr-44 sticky">
           <ListSections />
         </aside>
         <article className="flex flex-col items-center justify-center w-screen h-screen">
-        
-          <img src={oferta} className=" flex gap-2 ml-38 mt-48 h-72 w-9/12 " />
+          
+        <img
+            src={oferta}
+            className=" flex gap-2 ml-38 mt-48 h-72 w-9/12 hover:scale-105 "
+          />
           <div>
             <Carousel>
               <div>
