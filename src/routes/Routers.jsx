0bbea7a14677f8a214e3822aea257255e225d5b0
lucas fernandez <flucasyoel@outlook.es>
2024-02-24 22:05:27 -0300

    Add - Register Component and Add route

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import ListSections from "../components/ListSections";
 import CarouselHome from "../components/CarouselHome";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const Routers = () => {
   return (
@@ -12,6 +13,7 @@ const Routers = () => {
       <Route path="/Example" element={<ListSections />} />
       <Route path="/prueba" element={<CarouselHome />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
